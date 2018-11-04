88e588cf502e068d346b9697282c0da493b7fb12
SaiprasadDuduka <35596537+SaiprasadDuduka@users.noreply.github.com>
2018-11-04 10:03:15 +0530

    Add files via upload
    Game conditions added to the code

--- Pong.java
+++ Pong.java
@@ -10,14 +10,19 @@ public class Pong extends Applet implements Runnable,KeyListener{
 	Image dbImage;
 	Graphics dbg;
 	
+	//Game stages;
+	boolean intro = true;
+	boolean gameOver = false;
+	boolean space = false;
+	
 	//Reusable Values;
 	int rectWidth = 8;
 	int rectHeight = 80;
 	int player1Score = 0,player1ScoreX,player1ScoreY;
 	int player2Score = 0,player2ScoreX,player2ScoreY; 
-	int ballWidth = 16,ballHeight = 16,ballX = 400,ballY = 81;
+	int initBallX = 400,initBallY = 324,ballX = 400,ballY = 324,ballHeight=16,ballWidth=16;
 	int leftY = 264, rightY = 264;//Taking only y co-ordinates as it can only move in y-axis
-	int speed = 16;
+	int speed = 16,vx = 2,vy=2;
 	
 	//Shape Objects
 	Shape rectLeft = new Rectangle(8,leftY,rectWidth,rectHeight);
@@ -59,23 +64,61 @@ public class Pong extends Applet implements Runnable,KeyListener{
 	public void paint(Graphics g){
 		Graphics2D gd = (Graphics2D)g;
 		
-		//Border white rect as apple window size is changing
-		gd.setColor(Color.WHITE);
-		gd.drawRect(0,0,800,648);
-		
-		//Two rectangles:
-		gd.setColor(Color.BLUE);
-		gd.fill(rectLeft);
-		gd.fill(rectRight);
-		
-		//Score:
-		gd.setColor(Color.WHITE);
-		gd.drawString(String.valueOf(player1Score),8,16);
-		gd.drawString(String.valueOf(player2Score),784,16);
-		
-		//Ball:
-		gd.setColor(Color.YELLOW);
-		gd.fill(circle);
+		if(intro){//For Start Screen
+			g.setColor(Color.GREEN);
+			gd.drawString("Pong",392,248);
+			gd.drawString("Press 'Space' to Start",328,264); 
+			if(space){
+				intro = false;
+				space = false;
+			}
+		}else if(gameOver){//For end screen
+			gd.setColor(Color.RED);
+			gd.drawString("Press 'SPACE' to Continue",328,264);
+			if(space){
+				gameOver = false;
+				space = false;
+			}
+		}else{
+			
+			if(player1Score == 5){
+				gd.setColor(Color.GREEN);
+				gd.drawString("Player1 Wins",328,248);
+				gd.drawString("Press 'SPACE' to Continue",328,264);
+				if(space){
+					intro = true;
+					space = false;
+					player1Score = player2Score = 0;
+				}
+			}else if(player2Score == 5){
+				gd.setColor(Color.GREEN);
+				gd.drawString("Player2 Wins",328,248);
+				gd.drawString("Press 'SPACE' to Continue",328,264);
+				if(space){
+					intro = true;
+					space = false;
+					player1Score = player2Score = 0;
+				}
+			}else{
+				//Border white rect as apple window size is changing
+				gd.setColor(Color.WHITE);
+				gd.drawRect(0,0,800,648);
+				
+				//Two rectangles:
+				gd.setColor(Color.BLUE);
+				gd.fill(rectLeft);
+				gd.fill(rectRight);
+				
+				//Score:
+				gd.setColor(Color.WHITE);
+				gd.drawString(String.valueOf(player1Score),8,16);
+				gd.drawString(String.valueOf(player2Score),784,16);
+				
+				//Ball:
+				gd.setColor(Color.YELLOW);
+				gd.fill(circle);
+			}
+		}
 	}
 	
 	public void update(Graphics g){
@@ -86,6 +129,27 @@ public class Pong extends Applet implements Runnable,KeyListener{
 		paint(dbg);
 		g.drawImage(dbImage,0,0,this);
 		
+		//updating ball values
+		if(player1Score!=5 && player2Score!=5){
+			ballX += vx;
+			if(ballX < 8){
+				gameOver = true;
+				ballX = initBallX;
+				ballY = initBallY;
+				player2Score++;
+			}else if(ballX > 792){
+				gameOver = true;
+				ballX = initBallX;
+				ballY = initBallY;
+				player1Score++;
+			}
+			ballY += vy;
+			if(ballY < 8 || ballY > 640)
+				vy= vy * (-1);
+
+			//creating circle with updated values
+			circle = new Ellipse2D.Double(ballX,ballY,ballWidth,ballHeight);
+			}
 	}
 	
 	@Override
@@ -96,7 +160,7 @@ public class Pong extends Applet implements Runnable,KeyListener{
 			update(getGraphics());
 			
 			try{
-				Thread.sleep(15);
+				Thread.sleep(20);
 			}catch(Exception e){
 				System.out.println("Error Occured");
 			}
@@ -111,20 +175,34 @@ public class Pong extends Applet implements Runnable,KeyListener{
 			for(Character c : pressed){
 				switch(c)
 				{
+					case 25:
+						intro = true;
+						break;
+					case 32:
+						space = true;
+						break;
 					case 56:
-						rightY-=8;
+						rightY-=speed;
+						if(rightY < 0)
+							rightY = 0;
 						rectRight = new Rectangle(784,rightY,rectWidth,rectHeight);
 						break;
 					case 53:
-						rightY+=8;
+						rightY+=speed;
+						if(rightY > 648 - rectHeight)
+							rightY = 648 - rectHeight;
 						rectRight = new Rectangle(784,rightY,rectWidth,rectHeight);
 						break;
 					case 'w':
-						leftY-=8;
+						leftY-=speed;
+						if(leftY < 0)
+							leftY = 0;
 						rectLeft = new Rectangle(8,leftY,rectWidth,rectHeight);
 						break;
 					case 's':
-						leftY+=8;
+						leftY+=speed;
+						if(leftY > 648 - rectHeight)
+							leftY = 648 - rectHeight; 
 						rectLeft = new Rectangle(8,leftY,rectWidth,rectHeight);
 						break;
 				}
@@ -134,9 +212,8 @@ public class Pong extends Applet implements Runnable,KeyListener{
 		repaint();
 	}
 	
-	//We won't use this functions
 	@Override
-	public void keyReleased(KeyEvent e){
+	public synchronized void keyReleased(KeyEvent e){
 		pressed.remove(e.getKeyChar());
 	}
 	@Override
